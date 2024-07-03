5ef6f051877a5ee2413e6ba6fde16caa4bcd1961
Jonnathan23 <jsaquicela26@gmail.com>
2024-07-03 00:23:34 -0500

    initia lcommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FormularyComponent } from './components/formulary/formulary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'formulary', title:'formulary', component:FormularyComponent},
+    {path: '', redirectTo:'formulary', pathMatch:'full'}
+];
