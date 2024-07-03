7ca9bd5e7dd9631f46a6ca03cd0ad0d5d6ae03bb
Jonnathan23 <jsaquicela26@gmail.com>
2024-07-03 00:32:20 -0500

    Formulario añadido

--- src/app/components/formulary/formulary.component.ts
+++ src/app/components/formulary/formulary.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-formulary',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulary.component.html',
   styleUrl: './formulary.component.scss'
 })
 export class FormularyComponent {
+
+  login(form: NgForm) {    
+    const cedula = form.value.cedula;
+    console.log(cedula)
+
+  }
 
 }
